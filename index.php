3d797ff0899312a7ef019ed69ef0e124bbd3ad04
Nathan Trujillo <kelvin.beats@gmail.com>
2014-07-29 00:06:59 -0700

    Gmaps integration

--- index.php
+++ index.php
@@ -1,98 +1,124 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-    #tripmeter {
-        border: 1px solid grey;
-        padding: 10px;
-        margin: 10px 0;
-    }
-    p {
-        color: #222;
-        font: 14px Arial;
-    }
-    span {
-        color: #00C;
-    }
-</style>
-    <script src="http://modernizr.com/downloads/modernizr-latest.js"></script>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <style>
+        html { height: 50% }
+        body { height: 100%; margin: 0; padding: 0 }
+        #map-canvas { height: 100% }
+
+        #tripmeter {
+            border: 1px solid grey;
+            padding: 10px;
+            margin: 10px 0;
+        }
+        p {
+            color: #222;
+            font: 14px Arial;
+        }
+        span {
+            color: #00C;
+        }
+    </style>
+    <script src="//modernizr.com/downloads/modernizr-latest.js"></script>
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="/apps/fiveDriver/js/app.js"></script>
+    <script type="text/javascript" src="//maps.googleapis.com/maps/api/js?key=AIzaSyC0p2CXXzNyDGT3TB8fLsbkYmwzQmUi2eQ"></script>
+    <script type="text/javascript">
+        var mapOptions = {};
+        var map = {};
+        var version = ".1";
+        var UID = null;
+        var currentTrip = {};
+        var gpsOptions = {
+            enableHighAccuracy: false,
+            //timeout: 500,
+            maximumAge: 0
+        };
+
+        var route = [];
+
+
+        function errorHandler(error) {
+            console.log("Error occurred. Error code: " + error.code + " " + error.message);
+        }
+
+        window.onload = function() {
+            mapOptions = {
+                center: new google.maps.LatLng(32.750721399999996, -117.17515869999998),
+                zoom: 8
+            };
+
+            map = new google.maps.Map(document.getElementById("map-canvas"),
+                mapOptions);
+
+            if (Modernizr.localstorage) {//http://diveintohtml5.info/storage.html
+                localStorage.setItem("fiveDriver", version);
+                if(localStorage.getItem("UID") == null) {
+                    localStorage.setItem("UID",createUID(3));
+                }
+                UID = localStorage.getItem("UID");
+            } else {
+                alert("You might need to upgrade your browser for this to work.");
+            }
+
+            console.log("UID: " + localStorage.getItem("UID"));
+
+            var startPos;
+
+            if (navigator.geolocation) {
+
+                navigator.geolocation.getCurrentPosition(function(loc) {
+                    startPos = loc;
+                    document.getElementById("startLat").innerHTML = startPos.coords.latitude;
+                    document.getElementById("startLon").innerHTML = startPos.coords.longitude;
+                }, errorHandler);
+
+                var wpid = navigator.geolocation.watchPosition(function(position) {
+
+                    var now = new Date().getTime();
+                    var tmpObj = {};
+                    tmpObj.stamp = now;
+                    tmpObj.lat = position.coords.latitude;
+                    tmpObj.lon = position.coords.longitude;
+                    tmpObj.distance = calculateDistance(
+                        startPos.coords.latitude, startPos.coords.longitude,
+                        position.coords.latitude, position.coords.longitude
+                    );
+
+                    route.push(new google.maps.LatLng(tmpObj.lat, tmpObj.lon));
+
+                    console.log(route);
+
+                    var drivingRoute = new google.maps.Polyline({
+                        path: route,
+                        geodesic: true,
+                        strokeColor: '#FF0000',
+                        strokeOpacity: 1.0,
+                        strokeWeight: 2
+                    });
+
+                    drivingRoute.setMap(map);
+
+                    document.getElementById("currentLat").innerHTML = tmpObj.lat;
+                    document.getElementById("currentLon").innerHTML = tmpObj.lon;
+                    document.getElementById("distance").innerHTML = tmpObj.distance;
+
+                    console.log(tmpObj);
+
+                }, errorHandler, gpsOptions);//https://developer.mozilla.org/en-US/docs/Web/API/Geolocation.watchPosition
+            }
+        };
+    </script>
 </head>
 <body>
+<div id="map-canvas" style="width: 100%; height: 100%"></div>
 <div id="tripmeter">
-
-    <p>
-        Starting Location (lat, lon):<br/>
-        <span id="startLat">???</span>&deg;, <span id="startLon">???</span>&deg;
-    </p>
-    <p>
-        Current Location (lat, lon):<br/>
-        <span id="currentLat">???</span>&deg;, <span id="currentLon">???</span>&deg;
-    </p>
-    <p>
-        Distance from starting location:<br/>
-        <span id="distance">0</span> km
-    </p></div><script>
-
-    var version = ".1";
-    var UID = null;
-    var currentTrip = {};
-
-    if (Modernizr.localstorage) {//http://diveintohtml5.info/storage.html
-        localStorage.setItem("fiveDriver", version);
-        if(localStorage.getItem("UID") == null) {
-            localStorage.setItem("UID",createUID(3));
-        }
-        UID = localStorage.getItem("UID");
-    } else {
-        alert("Your browser does not support the features required to play planning poker:( Get Chrome or the Fox");
-    }
-
-    console.log("UID: " + localStorage.getItem("UID"));
-
-    var gpsOptions = {
-        enableHighAccuracy: false,
-        timeout: 500,
-        maximumAge: 0
-    };
-
-    window.onload = function() {
-
-        var startPos;
-
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                startPos = position;
-                document.getElementById("startLat").innerHTML = startPos.coords.latitude;
-                document.getElementById("startLon").innerHTML = startPos.coords.longitude;
-            }, function(error) {
-                alert("Error occurred. Error code: " + error.code);
-                // error.code can be:
-                //   0: unknown error
-                //   1: permission denied
-                //   2: position unavailable (error response from locaton provider)
-                //   3: timed out
-            });
-
-            navigator.geolocation.watchPosition(function(position) {
-
-                var now = new Date().getTime();
-                var tmpObj = {};
-                tmpObj.stamp = now;
-                tmpObj.lat = position.coords.latitude;
-                tmpObj.lon = position.coords.longitude;
-                tmpObj.distance = calculateDistance(startPos.coords.latitude, startPos.coords.longitude,
-                    position.coords.latitude, position.coords.longitude);
-
-                document.getElementById("currentLat").innerHTML = tmpObj.lat;
-                document.getElementById("currentLon").innerHTML = tmpObj.lon;
-                document.getElementById("distance").innerHTML = tmpObj.distance;
-                console.log(tmpObj);
-
-            }, function(){ console.log('fail')}, gpsOptions);//https://developer.mozilla.org/en-US/docs/Web/API/Geolocation.watchPosition
-        }
-    };
-</script>
+    <p>Starting Location (lat, lon):<br/>
+        <span id="startLat">???</span>&deg;, <span id="startLon">???</span>&deg;</p>
+    <p>Current Location (lat, lon):<br/>
+        <span id="currentLat">???</span>&deg;, <span id="currentLon">???</span>&deg;</p>
+    <p>Distance from starting location:<br/><span id="distance">0</span> km</p>
+</div>
 </body>
 </html>
